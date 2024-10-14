c394b9a34f39147ef2a509d654aa320ad10defa5
Olga Matosova <matosovaolga@gmail.com>
2024-10-14 11:31:20 +0300

    edit profile page

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ProfileHeaderComponent } from '../../common/profile-header/profile-header.component';
+import { ButtonComponent } from '../../common/button/button.component';
+import { ProfileService } from '../../data/services/profile.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ProfileHeaderComponent, ButtonComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
+  fb = inject(FormBuilder);
+  profileService = inject(ProfileService);
 
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: [{ value: '', disabled: true }, Validators.required],
+    description: [''],
+    stack: [''],
+  });
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue(this.profileService.me());
+    });
+  }
+
+  onSave = () => {
+	console.log('submit')
+    this.form.markAllAsTouched();
+    this.form.updateValueAndValidity();
+
+    if (this.form.invalid) return;
+    //@ts-ignore
+    firstValueFrom(this.profileService.patchProfile(this.form.value));
+  };
 }
